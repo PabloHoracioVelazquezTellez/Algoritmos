778c55dc4700dd7ea44ec8f91cbc0e5d16fa4d67
PabloHoracioVelazquezTelle <pablotz1504@gmail.com>
2024-09-19 14:22:00 -0600

    Agregar Titulo al Programa

--- catYearsDogYears.py
+++ catYearsDogYears.py
@@ -1,3 +1,7 @@
+"""
+Programa que calcula la edad de un gato
+y un perro con respecto a los años humanos
+"""
 def calculate_pet_years(humanYears):
     dogYears=0
     catYears=0
@@ -10,13 +14,11 @@ def calculate_pet_years(humanYears):
     else:
         catYears = 15 + 9 + (humanYears - 2) * 4
         dogYears = 15 + 9 + (humanYears - 2) * 5
-
     return {'Edad Humana': humanYears,
             'Edad en gatos': catYears,
             'Edad en perros': dogYears}
-
 #Para probar el programa
 humanYears = 10  # Para cambiar los resultados de la funcion
 print(calculate_pet_years(humanYears))
 print(type(calculate_pet_years(humanYears)))
-
+print("Primer cambio al codigo :DDD")
